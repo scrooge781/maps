edabf4d91b77994fb2f70a7ed1298a5dd9b64620
scrooge781 <42118036+scrooge781@users.noreply.github.com>
2021-09-29 12:27:33 +0300

    create main menu

--- app/src/main/java/com/oldmaps/newmaps/maps/MainActivity.kt
+++ app/src/main/java/com/oldmaps/newmaps/maps/MainActivity.kt
@@ -1,13 +1,42 @@
 package com.oldmaps.newmaps.maps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.Navigation
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
+
+        init()
+    }
+
+
+    private fun init() {
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.menu_modern_map, R.id.menu_vintage_map, R.id.menu_marker_map, R.id.menu_setting
+            ), drawer_layout
+        )
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        nav_view.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
