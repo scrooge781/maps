737cada54c5f1aba84908edc8d782a0ddfd09426
scrooge781 <42118036+scrooge781@users.noreply.github.com>
2021-11-05 13:05:20 +0200

    Add: -- method highlighted item recyclerview
    Основное: добавленый метод выделение items recyclerview

--- app/src/main/java/com/oldmaps/newmaps/maps/ui/menu/modern_map/adapter/ModerMapTypeAdapter.kt
+++ app/src/main/java/com/oldmaps/newmaps/maps/ui/menu/modern_map/adapter/ModerMapTypeAdapter.kt
@@ -1,5 +1,6 @@
 package com.oldmaps.newmaps.maps.ui.menu.modern_map.adapter
 
+import android.content.ClipData
 import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
@@ -13,23 +14,42 @@ import androidx.recyclerview.widget.RecyclerView
 import com.oldmaps.newmaps.maps.R
 import com.oldmaps.newmaps.maps.util.BaseRecyclerViewAdapter
 import kotlinx.android.synthetic.main.item_list_modern_map_type.view.*
+import android.content.ClipData.Item
+
 
 class ModerMapTypeAdapter : BaseRecyclerViewAdapter<ModernMapModel>() {
 
+    private var selectedPos = -1
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_list_modern_map_type, parent, false))
+        return MyViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_list_modern_map_type, parent, false)
+        )
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         var myHolder = holder as? MyViewHolder
         myHolder?.setUpView(modernMap = getItem(position))
         myHolder?.item?.setOnClickListener {
-            myHolder.focusable.setBackgroundResource(R.drawable.enable_element_modern_map)
-            myHolder.title.setTextColor(Color.parseColor("#FFAF38"))
+            selectedPos = position
+            notifyDataSetChanged()
         }
+
+        if(selectedPos == position){
+            myHolder?.focusable?.setBackgroundResource(R.drawable.enable_element_modern_map)
+            myHolder?.title?.setTextColor(Color.parseColor("#FFAF38"))
+        } else {
+            myHolder?.focusable?.setBackgroundResource(R.drawable.disable_element_modern_map)
+            myHolder?.title?.setTextColor(Color.parseColor("#FF170E0D"))
+        }
+
+
     }
 
-    inner class MyViewHolder (view: View) : RecyclerView.ViewHolder(view) {
+
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         val item: ConstraintLayout = view.itemModerMapType
         val image: ImageView = view.moderMapType
@@ -40,11 +60,13 @@ class ModerMapTypeAdapter : BaseRecyclerViewAdapter<ModernMapModel>() {
         fun setUpView(modernMap: ModernMapModel?) {
             image.setImageResource(modernMap?.image!!)
             title.text = modernMap.title
-            if(modernMap.focusable){
-                focusable.setBackgroundResource(R.drawable.enable_element_modern_map)
-                title.setTextColor(Color.parseColor("#FFAF38"))
-            }
+
+            /*  if(modernMap.focusable){
+                  focusable.setBackgroundResource(R.drawable.enable_element_modern_map)
+                  title.setTextColor(Color.parseColor("#FFAF38"))
+              }*/
 
         }
     }
+
 }
